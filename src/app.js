e3dd3e50b23af988209de81d019eea8ba1feb4c3
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-06-13 00:02:57 +0200

    added basic tasks handling

--- src/app.js
+++ src/app.js
@@ -6,25 +6,30 @@ const settingsTriggerEl = document.querySelector('.settings--trigger')
 
 const setLoading = active => loaderEl.classList.toggle('hidden', !active)
 
+const parseTasks = json => {
+  json = json.replace(/\w.*\[(\w+)\]/g, (m, m1) => m1.toLowerCase())
+  try {
+    window.dispatchEvent(new CustomEvent('tasks-loaded', { detail: JSON.parse(json) }))
+    return Promise.resolve('tasks-loaded')
+  } catch (err) {
+    return Promise.reject(new Error('failed at parsing json' + err))
   }
-  el.classList.add('fade')
-  el.style.opacity = isVisible ? 1 : 0
-  setTimeout(() => (el.classList.toggle('hidden', isVisible)), isVisible ? 300 : 10)
-  setTimeout(() => (el.style.opacity = isVisible ? 0 : 1), 20)
 }
 
-const setLoading = (active, infinite) => {
-  toggleVisibility(loaderEl, active)
-  if (infinite === undefined) {
-    setTimeout(() => setLoading(false), 1000)
-  }
+const loadTasks = api => {
+  storage.set('api', api)
+    .then(() => fetch(api))
+    .then(res => res.text())
+    .then(json => parseTasks(json))
+    .catch(err => console.error(err))
+    .then(() => setLoading(false))
 }
 
 window.addEventListener('settings-submitted', (event) => {
   console.log('new settings submitted :', event.detail)
   setLoading(true)
   settingsTriggerEl.classList.remove('action-required')
-  storage.set('api', event.detail.api)
+  loadTasks(event.detail.api)
 })
 
 setTimeout(() => {
@@ -35,6 +40,6 @@ setTimeout(() => {
     })
     .catch(() => {
       console.log('found no api in storage')
-    settingsTriggerEl.classList.add('action-required')
-})
+      settingsTriggerEl.classList.add('action-required')
+    })
 }, 100)
