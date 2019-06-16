d950dd8a3ce151f822a603ed0a9fea84a6a71305
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-06-16 19:21:29 +0200

    update airtable after mark as done

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import * as storage from './plugins/storage'
 class App {
   constructor () {
     this.loaderEl = document.querySelector('.loader')
+    this.apiBase = null
+    this.apiKey = null
+    this.tasksLoaded = false
     this.setupListeners()
     this.recoverApi()
   }
@@ -19,6 +22,8 @@ class App {
     window.addEventListener('set-loading', event => this.setLoading(event.detail))
     window.addEventListener('api-response', event => this.parseApiResponse(event.detail))
     window.addEventListener('api-set', event => this.onApiSet(event.detail.base, event.detail.key))
+    window.addEventListener('task-update', event => this.onTaskUpdate(event.detail))
+    window.addEventListener('tasks-loaded', () => (this.tasksLoaded = true))
   }
   async emit (eventName, eventData) {
     console.log(`%c${eventName}`, 'color: blue', eventData)
@@ -26,16 +31,24 @@ class App {
   }
   onApiSet (base, key) {
     return this.loadTasks(base, key)
-      .then(() => storage.set('api-base', base))
-      .then(() => storage.set('api-key', key))
+      .then(() => {
+        this.apiBase = base
+        return storage.set('api-base', base)
+      })
+      .then(() => {
+        this.apiKey = key
+        return storage.set('api-key', key)
+      })
   }
   recoverApi () {
     this.sleep(10)
       .then(() => Promise.all([storage.get('api-base'), storage.get('api-key')]))
       .then(([base, key]) => this.emit('api-set', { base, key }))
       .catch(() => {
-        this.emit('show-toast', { type: 'info', message: 'please setup api in settings' })
-        this.emit('action-required', true)
+        if (!this.tasksLoaded) {
+          this.emit('show-toast', { type: 'info', message: 'please setup api in settings' })
+          this.emit('action-required', true)
+        }
       })
   }
   loadTasks (apiBase, apiKey) {
@@ -82,6 +95,29 @@ class App {
       console.error(err)
       return Promise.reject(new Error('api does not return the expected format'))
     }
+  }
+  patch (url, data) {
+    return fetch(url, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'patch',
+      body: JSON.stringify(data),
+    })
+  }
+  onTaskUpdate (task) {
+    if (this.apiBase === null || this.apiKey === null) {
+      return this.showError('cannot update task without api')
+    }
+    const url = `https://api.airtable.com/v0/${this.apiBase}/tasks/${task.id}?api_key=${this.apiKey}&view=todo`
+    return this.setLoading(true)
+      .then(() => this.patch(url, { fields: { 'completed-on': task['completed-on'] } }))
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .then(() => this.sleep(500))
+      .catch(err => this.showError(err.message))
+      .then(() => this.setLoading(false))
   }
 }
 // eslint-disable-next-line no-new
