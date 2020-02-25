c3ad8a961585bf87cd85e5623242daca4a8f980f
Romain Racamier <romain.racamier@externe.e-i.com>
2020-02-25 19:02:37 +0100

    simpler storage

--- src/plugins/storage.js
+++ src/plugins/storage.js
@@ -1,17 +1,7 @@
 
-import pkg from '../../package.json'
-
-const separator = '_'
-const baseKey = `${pkg.name}${separator}${pkg.version}`
-
-console.log('local storage will use base key :', baseKey)
-
 export async function get (key) {
-  const fullKey = baseKey + separator + key
-  const data = localStorage[fullKey]
-  if (!data) {
-    return Promise.reject(new Error(`storage : found no matching key "${fullKey}"`))
-  }
+  const data = localStorage[key]
+  if (!data) return Promise.reject(new Error(`storage : found no matching key "${key}"`))
   try {
     return Promise.resolve((data[0] === '{') ? JSON.parse(data) : data)
   } catch (e) {
@@ -20,8 +10,7 @@ export async function get (key) {
 }
 
 export async function set (key, data) {
-  const fullKey = baseKey + separator + key
-  localStorage[fullKey] = typeof data === 'object' ? JSON.stringify(data) : data
+  localStorage[key] = typeof data === 'object' ? JSON.stringify(data) : data
   return Promise.resolve(data)
 }
 
