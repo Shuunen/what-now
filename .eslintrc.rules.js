4d912a3c38fb3c654d03377a2d1ff5ae37d3af27
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-12-28 02:46:38 +0100

    from eslint to xo

--- .eslintrc.rules.js
+++ .eslintrc.rules.js
@@ -4,5 +4,8 @@
 module.exports = {
   'no-console': 'off',
   'func-names': ['error', 'always'],
-  'comma-dangle': ['error', 'always-multiline'],
+  'object-curly-spacing': ['error', 'always'],
+  curly: ['error', 'multi'],
+  'capitalized-comments': 'off',
+  '@typescript-eslint/consistent-type-assertions': 'off'
 }
