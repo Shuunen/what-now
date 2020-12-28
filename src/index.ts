ae1fe4c682aff27b01234928d6ea28155d3f4d6b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-12-28 02:52:13 +0100

    from parcel js to poi ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import './assets/style.css'
+import { landing } from './views'
+
+const app = document.querySelector('#app')
+
+if (app) {
+  app.className = 'flex flex-col min-h-full items-center justify-center p-6 bg-gray-900 text-blue-100'
+  app.append(landing)
+}
