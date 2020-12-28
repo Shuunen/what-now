90ee4e132c84ef3124e28d6a91123db41f5808bb
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-12-28 02:49:47 +0100

    use tailwind with production purge

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    content: ['./src/**/*.ts']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
