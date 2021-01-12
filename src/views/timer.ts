2afae1b469da71d846ebd7b54dab1d38133fdfca
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-01-12 18:16:47 +0100

    refresh the timer on task update

--- src/views/timer.ts
+++ src/views/timer.ts
@@ -4,14 +4,27 @@ import { div } from '../utils'
 
 export const timer = div('timer font-thin bottom-4 fixed right-4 text-6xl text-gray-700')
 
+let minutes = 0
+
+const updateTimer = () => {
+  timer.textContent = minutes > 0 ? `${minutes}min` : ''
+}
+
 const onTaskLoaded = (tasks: Task[]) => {
   let seconds = 0
   tasks.forEach(task => {
     if (!task.isActive()) return
     seconds += task.averageTime
   })
-  const minutes = Math.round(seconds / 60)
-  timer.textContent = minutes > 0 ? `${minutes}min` : ''
+  minutes = Math.round(seconds / 60)
+  updateTimer()
+}
+
+const onTaskUpdate = (task: Task) => {
+  const seconds = (task.isActive() ? 1 : -1) * task.averageTime
+  minutes += Math.round(seconds / 60)
+  updateTimer()
 }
 
 on('tasks-loaded', onTaskLoaded)
+on('task-update', onTaskUpdate)
