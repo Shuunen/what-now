356a4373f07cf3fd16c2a40cb65d4d2375f62d52
Romain Racamier <romain.racamier@externe.e-i.com>
2021-01-21 09:51:55 +0100

    avoid stacking notif & timer in the same spot

--- src/views/notifications.ts
+++ src/views/notifications.ts
@@ -1,7 +1,7 @@
 import { emit, on, sleep } from 'shuutils'
 import { dom } from '../utils'
 
-export const notification = dom('button', '🔔', 'fixed text-4xl right-6 bottom-6 opacity-0 transition duration-300 ease-in-out cursor-pointer')
+export const notification = dom('button', '🔔', 'fixed text-4xl right-6 top-6 opacity-0 transition duration-300 ease-in-out cursor-pointer')
 
 notification.addEventListener('click', () => {
   emit('ask-notification-perm')
