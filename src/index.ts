e91c0422dad531b12061313ef6ee84c39a5aa091
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-06-26 16:43:14 +0200

    from twind to shuunen-stack

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { setup as twind } from 'twind/shim'
 import { credentialService, idleService, tasksService, workerService } from './services'
+import './styles.css'
 import { landing } from './views/landing'
 
-twind({ mode: 'silent' })
 document.body.append(landing)
 
 credentialService.init()
