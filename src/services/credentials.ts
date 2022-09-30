c9ba527e4a816346766d9e2e8ce73b1fc259d8ce
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-09-30 10:44:56 +0200

    bump shuutils

--- src/services/credentials.ts
+++ src/services/credentials.ts
@@ -1,8 +1,8 @@
 import { emit, on, storage } from 'shuutils'
 
 interface Credentials {
-  base: string;
-  key: string;
+  base: string
+  key: string
 }
 
 const DEMO_BASE = 'appQaesCng5o5xqE2'
@@ -16,23 +16,25 @@ class CredentialService {
     on('save-credentials', async (credentials: Credentials) => this.save(credentials))
   }
 
-  async checkStorage (): Promise<boolean | undefined> {
-    const base: string = (await storage.get('api-base')) ?? ''
-    const key: string = (await storage.get('api-key')) ?? ''
+  async checkStorage (): Promise<boolean> {
+    const base = storage.get('api-base', '')
+    const key = storage.get('api-key', '')
     const ok = this.validate(base, key)
     if (!ok) return emit('need-credentials')
     this.use({ base, key })
+    return true
   }
 
-  async checkHash (): Promise<boolean | undefined> {
+  async checkHash (): Promise<boolean> {
     const { hash } = document.location
-    const matches = /#(app\w{14})&(key\w{14})/.exec(hash) ?? []
-    if (matches.length !== 3) return this.checkStorage()
+    const matches = /#(app\w{14})&(key\w{14})/.exec(hash)
+    if (!matches || matches.length !== 3 || !matches[1] || !matches[2]) return this.checkStorage()
     document.location.hash = ''
-    return this.save({ base: matches[1], key: matches[2] })
+    await this.save({ base: matches[1], key: matches[2] })
+    return true
   }
 
-  async save (credentials: Credentials): Promise<boolean | undefined> {
+  async save (credentials: Credentials): Promise<boolean> {
     const ok = this.validate(credentials.base, credentials.key)
     if (!ok) return emit('need-credentials')
     if (credentials.base !== DEMO_BASE) {
@@ -40,6 +42,7 @@ class CredentialService {
       await storage.set('api-key', credentials.key)
     }
     this.use(credentials)
+    return true
   }
 
   validate (base: string, key: string): boolean {
