3375ff340f2b5ba947dc8ae923e0b58e0cd00ba8
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-10-02 23:45:40 +0200

    add storage prefix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { storage } from 'shuutils'
 import { credentialService, idleService, tasksService, workerService } from './services'
 import { landing } from './views/landing'
+
+storage.prefix = 'what-now_'
 
 document.body.append(landing)
 
