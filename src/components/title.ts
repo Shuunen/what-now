5ff608b84209e4dbefdf679481dbf01bbbce0a2c
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-01-30 02:02:01 +0100

    disable import rules

--- src/components/title.ts
+++ src/components/title.ts
@@ -1,5 +1,4 @@
 import { h1, tw } from 'shuutils'
-// eslint-disable-next-line import/extensions
 import pkg from '../../package.json'
 
 const title = h1(tw('app-title -ml-2 mb-4 text-5xl text-blue-300 sm:text-7xl'), 'What now')
