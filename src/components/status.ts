6d462019fea5250efd595fb9b2399af84ea2eefb
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-03-21 23:50:47 +0100

    avoid content move while loading

--- src/components/status.ts
+++ src/components/status.ts
@@ -4,8 +4,11 @@ import { state, watchState } from '../utils/state'
 const error = text(tw('app-error font-bold text-red-500'), state.statusError)
 watchState('statusError', () => { error.innerHTML = state.statusError })
 
-const info = text('info', state.statusInfo)
-watchState('statusInfo', () => { info.innerHTML = state.statusInfo })
+const info = text('info text-transparent transition-colors ease-in-out', state.statusInfo)
+watchState('statusInfo', () => {
+  info.innerHTML = state.statusInfo || '&nbsp;'
+  info.classList.toggle(tw('text-inherit'), state.statusInfo !== '')
+})
 
 const progress = text(tw('app-progress text-2xl font-light italic'), state.statusProgress)
 watchState('statusProgress', () => { progress.innerHTML = state.statusProgress })
