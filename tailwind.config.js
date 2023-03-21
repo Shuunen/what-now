def4194f1cfe72512fb9bf6264eb6d8c7b771464
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-03-21 23:18:48 +0100

    use dep cruiser

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
-/* eslint-disable putout/putout */
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line import/unambiguous, import/no-commonjs
 module.exports = {
   content: ['./public/index.html', 'src/**/*.ts'],
 }
