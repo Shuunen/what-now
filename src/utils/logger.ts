8b703ceabfb42469ec4f5a72b48650ced91ad147
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-06-02 16:17:31 +0200

    enhance stuff to message transformation

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,22 @@
 import { LogLevel, Logger } from 'shuutils'
 import { state } from './state'
 
+function stuffToMessage (...stuff: unknown[]): string {
+  return stuff.map(thing => {
+    if (typeof thing === 'string') return thing
+    if (typeof thing === 'object') return JSON.stringify(thing)
+    return String(thing)
+  }).join(', ')
+}
+
 /* c8 ignore next 5 */
 class CustomLogger extends Logger {
   public override error (...stuff: unknown[]): void {
     super.error(...stuff)
-    state.showErrorToast = stuff.join(', ')
+    state.showErrorToast = stuffToMessage(...stuff)
   }
 }
 
 const logger = new CustomLogger({ willOutputToConsole: typeof window !== 'undefined', minimumLevel: LogLevel.Info })
 
-export { logger }
+export { logger, stuffToMessage }
---
+++ tests/logger.test.ts
@@ -0,0 +1,17 @@
+import { expect, it } from 'vitest'
+import { stuffToMessage } from '../src/utils/logger'
+
+it('stuffToMessage A', function () {
+  const result = stuffToMessage('a', 'b', 'c')
+  expect(result).toBe('a, b, c')
+})
+
+it('stuffToMessage B', function () {
+  const result = stuffToMessage('a', 'b', { cat: 'd' })
+  expect(result).toMatchInlineSnapshot('"a, b, {\\"cat\\":\\"d\\"}"')
+})
+
+it('stuffToMessage C', function () {
+  const result = stuffToMessage('a', 'b', { cat: 'dog' }, 1, 2, 3)
+  expect(result).toMatchInlineSnapshot('"a, b, {\\"cat\\":\\"dog\\"}, 1, 2, 3"')
+})
