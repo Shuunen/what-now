991f0fe05904679397f448e0f4765ea9d4958b17
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-06-02 16:16:46 +0200

    enhance hide toast logic

--- src/components/toasts.ts
+++ src/components/toasts.ts
@@ -4,7 +4,7 @@ import { state, watchState } from '../utils/state'
 const toasts = div('app-toasts fixed bottom-3 z-10 right-5 select-none ml-5')
 
 function buildToast (type: LogLevel, message: string) {
-  const toast = div(tw('app-toast mt-2 rounded-lg bg-gradient-to-tr p-4 opacity-0 shadow-lg transition-opacity delay-300 ease-in-out'))
+  const toast = div(tw('app-toast mt-2 rounded-lg bg-gradient-to-tr p-4 opacity-0 shadow-lg transition-all delay-300 ease-in-out'))
   const icon = div(tw('relative -top-0.5 float-left opacity-60 saturate-0'), type === LogLevel.Error ? '⚠' : 'ℹ️')
   const text = div(tw('ml-1 inline'), ellipsis(message, 100))  // eslint-disable-line @typescript-eslint/no-magic-numbers
   toast.append(icon, text)
@@ -12,15 +12,21 @@ function buildToast (type: LogLevel, message: string) {
   return toast
 }
 
+async function hideToast (toast: HTMLElement) {
+  toast.classList.add(tw('translate-x-16'))
+  await sleep(10) // eslint-disable-line @typescript-eslint/no-magic-numbers
+  toast.classList.remove(tw('opacity-100'))
+  setTimeout(() => { toast.remove() }, 500) // eslint-disable-line @typescript-eslint/no-magic-numbers
+}
+
 async function showToast (type: LogLevel, message: string, delay = 5000) {
   const toast = buildToast(type, message)
   if (type === LogLevel.Error) toast.classList.add(tw('from-red-950/70'), tw('to-red-900/50'))
   else toast.classList.add(tw('from-gray-950/70'), tw('to-gray-900/50'))
   setTimeout(() => { toast.classList.add(tw('opacity-100')) }, 100) // eslint-disable-line @typescript-eslint/no-magic-numbers
-  toast.addEventListener('click', () => { toast.classList.remove(tw('opacity-100')) })
+  toast.addEventListener('click', () => { void hideToast(toast) })
   await sleep(delay)
-  toast.classList.remove(tw('opacity-100'))
-  setTimeout(() => { toast.remove() }, 1000) // eslint-disable-line @typescript-eslint/no-magic-numbers
+  void hideToast(toast)
 }
 
 watchState('showErrorToast', () => { void showToast(LogLevel.Error, state.showErrorToast) })
