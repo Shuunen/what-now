ae3686edc899738ddbe8a0c87190a82a3ab8dc88
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-04-14 18:47:51 +0200

    change home network detection approach

--- src/utils/network.utils.ts
+++ src/utils/network.utils.ts
@@ -2,28 +2,12 @@
 import { logger } from './logger.utils'
 import { state } from './state.utils'
 
-// eslint-disable-next-line @microsoft/sdl/no-insecure-url, sonar/no-clear-text-protocols
-async function isHomeNetwork (checkUrl = 'http://192.168.0.1') {
-  // eslint-disable-next-line promise/avoid-new
-  return await new Promise<boolean>((resolve) => {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', checkUrl, true)
-    xhr.timeout = 300
-    xhr.addEventListener('load', function onLoadCallback () {
-      if (xhr.status === 200) logger.info('Feels like home :) connection successful, device is reachable') // eslint-disable-line @typescript-eslint/no-magic-numbers
-      else logger.info(`Feels like home :) connection error, ${xhr.statusText}, device is reachable`)
-      resolve(true)
-    })
-    xhr.onerror = function onErrorCallback () { // eslint-disable-line func-name-matching, unicorn/prefer-add-event-listener
-      logger.info('Feels like home :) connection failed, device seems reachable and seems to reject the connection')
-      resolve(true)
-    }
-    xhr.ontimeout = function onTimeoutCallback () { // eslint-disable-line func-name-matching
-      logger.info('Does not feels like home :( Connection timed out, device is not reachable')
-      resolve(false)
-    }
-    xhr.send()
-  })
+async function isHomeNetwork () {
+  // because of CORS, localhost cannot reach ipapi.co and therefore it means we are in home network
+  const response = await fetch('https://ipapi.co/version').catch(() => new Response('IPv4'))
+  const version = await response.text()
+  // if we are in home network, we are in IPv4, otherwise we are in IPv6
+  return version === 'IPv4'
 }
 
 export async function checkHomeNetwork () {
