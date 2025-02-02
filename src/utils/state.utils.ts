7eef059e33d927df0590e0e8bb4f73ed4a2b34fb
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-02-02 16:14:18 +0100

    chore(state): add comments to describe some props

--- src/utils/state.utils.ts
+++ src/utils/state.utils.ts
@@ -8,13 +8,16 @@ export const { state, watchState } = createState({
   apiDatabase: '',
   hueEndpoint: '',
   isLoading: false,
+  /** true if the app is ready to use */
   isSetup: false,
   showErrorToast: '',
   showInfoToast: '',
   statusError: '',
   statusInfo: 'Loading, please wait...',
   statusProgress: '',
+  /** the last list of tasks fetched */
   tasks,
+  /** timestamp of the last time tasks were fetched, in milliseconds */
   tasksTimestamp: 0,
 }, storage, /* c8 ignore next */['apiDatabase', 'apiCollection', 'hueEndpoint', 'tasks', 'tasksTimestamp'])
 
