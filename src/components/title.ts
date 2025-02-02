4a33b66c8d1e075df955d0e03c4e078840195f88
Romain Racamier-Lafon <romain.racamier@gmail.com>
2025-02-02 22:48:25 +0100

    fix: rely on unique-mark instead of pkg.json (-2kb in the build)

--- src/components/title.ts
+++ src/components/title.ts
@@ -1,8 +1,6 @@
 import { h1, tw } from 'shuutils'
-// eslint-disable-next-line unicorn/prevent-abbreviations
-import pkg from '../../package.json'
 
 const title = h1(tw('app-title -ml-2 text-5xl text-blue-300 sm:text-7xl'), 'What now')
-title.title = `v${pkg.version}`
+title.title = '__unique-mark__'
 
 export { title }
