1e3b4374709cf96d252b1a96b3a7815ce98a5db9
Romain Racamier-Lafon <romain.racamier@gmail.com>
2026-07-14 12:16:06 +0200

    feat(page): enhance About page

--- src/pages/page-about.tsx
+++ src/pages/page-about.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { FloatingMenu } from '../components/floating-menu'
 import { useActions } from '../utils/pages.utils'
 
@@ -5,18 +6,23 @@ export function PageAbout() {
   const actions = useActions()
   return (
     <div className="flex grow flex-col justify-center gap-4 text-center" data-testid="page-about">
-      <h1>About</h1>
+      <h1 className="mb-4">About</h1>
       <p>
         This webapp has been deployed from this open-source code{' '}
-        <a className="border-b" href="https://github.com/Shuunen/what-now" rel="noopener noreferrer" target="_blank">
+        <a className="border-b" data-testid="link-github" href="https://github.com/Shuunen/what-now" rel="noopener noreferrer" target="_blank">
           on Github
         </a>
         .
-        <br />
-        <br />
-        Please check the above link to be introduced to this app : what is it and how to use it.
       </p>
-      <p>__unique-mark__</p>
+      <p>Please check the above link to be introduced to this app : what is it and how to use it.</p>
+      <p>
+        Curious about the UI components ? Check out the{' '}
+        <Link className="border-b" data-testid="link-kitchen-sink" to="/kitchen-sink">
+          kitchen sink
+        </Link>
+        .
+      </p>
+      <small>__unique-mark__</small>
       <FloatingMenu actions={actions} />
     </div>
   )
